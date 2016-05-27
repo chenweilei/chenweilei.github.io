0ff1625aabdab78408041eaa3939e3f850a7a658
chenweilei <you@example.com>
2016-05-27 09:53:30 +0800

    delAll

--- example/ICBC/wxConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "jssdk.php";
-$jssdk = new JSSDK("wx996d3493e3952f71", "33ff921c459398454c1a6fd616d70688");
-$signPackage = $jssdk -> GetSignPackage();
-echo json_encode($signPackage);
-
-?>
