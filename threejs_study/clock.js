bad24bbec35f864a3277e557a12245c96acac607
chenweilei <cwl_cx@sina.com>
2017-02-14 17:30:27 +0800

    threejs_canvas纹理修改

--- threejs_study/clock.js
+++ threejs_study/clock.js
@@ -7,13 +7,21 @@ var canvas;
     canvas = document.createElement('canvas');
     canvas.width = 200;
     canvas.height = 200;
+    // canvas.style.position = 'fixed';
+    // canvas.style.top = '0px';
+    // canvas.style.left = '0px';
+    // document.body.appendChild(canvas);
     var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0,0,200, 200);
 
     function drawCir(){
         ctx.save()
         ctx.translate(100, 100)
+        ctx.strokeStyle = '#f00';
         ctx.lineWidth = 10;
-        ctx.arc(0,0, 100, 0, 360*Math.PI/180,false);
+        ctx.arc(0,0, 95, 0, 360*Math.PI/180,false);
         ctx.stroke();
         ctx.restore()
     }
