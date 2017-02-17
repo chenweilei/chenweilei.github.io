4a0b6200a40f165d68b0861eaf7e96f623e44227
chenweilei <cwl_cx@sina.com>
2017-02-17 18:25:15 +0800

    threejs学习

--- threejs_study/clock.js
+++ threejs_study/clock.js
@@ -7,17 +7,17 @@ var canvas;
     canvas = document.createElement('canvas');
     canvas.width = 200;
     canvas.height = 200;
+    var initDeg = 0;
     // canvas.style.position = 'fixed';
     // canvas.style.top = '0px';
     // canvas.style.left = '0px';
     // document.body.appendChild(canvas);
     var ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(0,0,200, 200);
-
+    //画圆盘
     function drawCir(){
         ctx.save()
+        ctx.beginPath()
         ctx.translate(100, 100)
         ctx.strokeStyle = '#f00';
         ctx.lineWidth = 10;
@@ -25,5 +25,25 @@ var canvas;
         ctx.stroke();
         ctx.restore()
     }
-    drawCir()
+    function drawPointer(){
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate(100, 100);
+        ctx.strokeStyle = '#00f';
+        ctx.lineWidth = 3;
+        ctx.moveTo(0,0);
+        ctx.lineTo(Math.sin(degToRad(initDeg-180))*100,Math.cos(degToRad(initDeg-180))*100)
+        ctx.stroke()
+        ctx.restore();
+    }
+
+    setInterval(function(){
+        ctx.clearRect(0,0, 200, 200)
+        initDeg++;
+        initDeg%=360;
+        ctx.fillStyle = '#fff';
+        ctx.fillRect(0,0,200, 200);
+        drawCir()
+        drawPointer()
+    }, 1000/60)
 })()
